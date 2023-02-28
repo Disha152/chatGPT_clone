833f81b16f9dcf0b5955eb2c461c6cec3f8d405f
Disha gupta <109867094+Disha152@users.noreply.github.com>
2023-02-28 23:53:58 +0530

    My version chatGPT clone

---
+++ android/app/src/main/kotlin/com/example/chatgpt_mic_version/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatgpt_mic_version
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant/constant.dart
@@ -0,0 +1,1 @@
+  const apiKey = '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';
